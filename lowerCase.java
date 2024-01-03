9d9f7ff913ce40739abdefe114d3ae423567d944
lore345 <154428144+lore345@users.noreply.github.com>
2024-01-03 16:22:39 +0200

    Add files via upload

---
+++ lowerCase.java
@@ -0,0 +1,19 @@
+/** String processing exercise 1. */
+public class lowerCase {
+    public static void main(String[] args) {
+        String str = args[0];
+        System.out.println(lowerCase(str));
+    }
+
+    /**
+     * Returns a string which is identical to the original string,
+     * except that all the upper-case letters are converted to lower-case letters.
+     * Non-letter characters are left as is.
+     */
+    public static String lowerCase(String s) {
+        s = s.toLowerCase();
+
+        
+        return s;
+    }
+}
