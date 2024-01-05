b4048427240868d831c37ff125e8e3cbcbc45115
Loren Bar <lorenbar@Lorens-MacBook-Pro.local>
2024-01-05 12:31:06 +0200

    my hw

--- LowerCase.java
+++ LowerCase.java
@@ -1,4 +1,6 @@
-/** String processing exercise 1. */
+/**
+ * String processing exercise 1.
+ */
 public class LowerCase {
     public static void main(String[] args) {
         String str = args[0];
@@ -11,9 +13,20 @@ public class LowerCase {
      * Non-letter characters are left as is.
      */
     public static String lowerCase(String s) {
-        s = s.toLowerCase();
+        String res = "";
+        for (int i = 0; i < s.length(); i++) {
 
-        
+            // Check if the character is an upper-case letter
+            if (s.charAt(i) > 64 && s.charAt(i) < 91) {
+                // Convert to lower-case using ASCII values
+                res += (char) (s.charAt(i) + 32);
+            } else {
+                res += s.charAt(i);
+            }
+        }
+
         return s;
     }
+
 }
+
